ef77e6f66a7480fc72adbe22e8f267c6810c4155
Kuldeep Keshwar <kuldeepkeshwar@gmail.com>
2016-01-02 01:48:22 +0530

    init repo

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name ui.logger
+ * @description
+ * # ui.logger
+ *
+ * Main module of the application.
+ */
+angular
+  .module('ui.logger', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ui.logger.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ui.logger
+ */
+angular.module('ui.logger')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
